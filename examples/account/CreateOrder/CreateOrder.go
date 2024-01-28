6e77c7132471ac0b70e0f7c834016f0aa53e553d
Thanatos625 <53264913+Thanatos625@users.noreply.github.com>
2024-01-28 21:27:24 +0200

    create order ok

--- examples/account/CreateOrder/CreateOrder.go
+++ examples/account/CreateOrder/CreateOrder.go
@@ -19,8 +19,8 @@ func NewOrder() {
 	client := binance_futures_connector.NewClient(apiKey, secretKey, baseURL)
 
 	// Binance New Order endpoint - POST /fapi/v1/order
-	newOrder, err := client.NewCreateOrderService().Symbol("BTCUSDT").Side("BUY").PositionSide("LONG").Type("MARKET").
-		TimeInForce("GTC").Quantity(300).Price(39500).ClosePosition("false").WorkingType("MARK_PRICE").
+	newOrder, err := client.NewCreateOrderService().Symbol("BTCUSDT").Side("BUY").PositionSide("LONG").Type("LIMIT").
+		Quantity(0.004).TimeInForce("GTC").Price(39200).
 		Do(context.Background())
 	if err != nil {
 		fmt.Println(err)
